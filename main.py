2739774c26025c49e2d468672e2d649d935384de
divyangp063 <prajapati.30@buckeyemail.osu.edu>
2024-10-23 19:47:30 -0400

    Parsing and cleaning unnecessary tags

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import streamlit as st
+from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a website URL: ")
 
 if st.button("Scrape Site"):
     st.write("Scraping in process")
+    result = scrape_website(url)
+    body_content = extract_body_content(result)
+    clean_content = clean_body_content(body_content)
+    st.session_state.dom_content = clean_content
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", clean_content, height = 300)
