4a51f9e8a706da0ff6a38cb858ab57a6f9805991
divyangp063 <prajapati.30@buckeyemail.osu.edu>
2024-10-23 20:36:45 -0400

    Added model 'Moondream'

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
+from parse import parse_with_ollama
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a website URL: ")
@@ -13,3 +14,13 @@ if st.button("Scrape Site"):
 
     with st.expander("View DOM Content"):
         st.text_area("DOM Content", clean_content, height = 300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what do you want to parse?")
+
+    if st.button("Begin parsing"):
+        if parse_description:
+            st.write("Parsing the content")
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
